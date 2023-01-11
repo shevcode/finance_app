14c55d11259878037de33235854591e8aa14974a
Ivan Shevtsov <shevtsov.yan@gmail.com>
2023-01-11 18:00:45 +0200

    added validations for models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
     has_many :operations
+    validates :name, :description, presence: true
+    validates :name, uniqueness: true
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,5 @@
 class Operation < ApplicationRecord
   belongs_to :category
+  validates :amount, :odate, :description, presence: true
+  validates :amount, numericality: { greater_than: 0 }
 end
