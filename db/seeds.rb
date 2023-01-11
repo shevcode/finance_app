c1a610e49eb9f146d577fa75f8cf3c31586b89aa
Ivan Shevtsov <shevtsov.yan@gmail.com>
2023-01-11 17:31:02 +0200

    added seed with faker for operations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+10.times do
+    Operation.create(
+        amount: Faker::Number.decimal(l_digits: 2, r_digits: 2),
+        odate: Faker::Date.backward(days: 10),
+        description: Faker::Quote.yoda,
+        category_id: Faker::Number.between(from: 1, to: 3)
+    )
+end
