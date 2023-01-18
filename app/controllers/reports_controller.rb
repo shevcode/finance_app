bdd665313621a77f59af994a7ed28f6013d9dbbf
Ivan Shevtsov <shevtsov.yan@gmail.com>
2023-01-18 21:48:18 +0200

    add filter to ReportsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,25 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[ report_by_category report_by_dates ]
+
   def index
   end
 
   def report_by_category
+
   end
 
   def report_by_dates
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report_params = params
+
+  end
+
+  # Only allow a list of trusted parameters through. DO I NEED THIS METHOD TO CHECK PARAMETERS FROM REQUEST?
+  def report_params
+    params.permit(:start, :end)
   end
 end
