f2f2bf29fac857990d44038b0aa577e7b607c230
Ivan Shevtsov <shevtsov.yan@gmail.com>
2023-01-25 12:58:09 +0200

    Complete table in report_by_category view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -20,7 +20,7 @@ class ReportsController < ApplicationController
     @report = ReportByCategory.new(@report_params)
     if @report.valid?
       @report.category_groups = Operation.joins(:category).where(otype: @report.otype_id, odate: @report.start_date..@report.end_date).group(:name).sum(:amount)
-      @report.total = @report.category_groups.sum { |_, a| a }  
+      @report.total = @report.category_groups.sum { |_, a| a }.round(2)  
       @report.otype_name = Otype.find(@report.otype_id).title
       render :report_by_category
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_20_113901) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_104044) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_113901) do
   end
 
   create_table "operations", force: :cascade do |t|
-    t.decimal "amount"
+    t.decimal "amount", precision: 10, scale: 2
     t.datetime "odate"
     t.string "description"
     t.integer "category_id", null: false
