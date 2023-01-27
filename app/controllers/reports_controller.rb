59552de5b7cf2b083a5b7bb66c598e4af20c3886
Ivan Shevtsov <shevtsov.yan@gmail.com>
2023-01-27 14:49:03 +0200

    Add basic features to report_by_dates

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[ report_by_category report_by_dates ]
   class Report
     include ActiveModel::Model
-    attr_accessor :start_date, :end_date, :otype_name, :otype_id, :category_id, :category_groups, :total
+    attr_accessor :start_date, :end_date, :otype_name, :otype_id, :category_id, :category_name, :category_groups, :total
     validates :start_date, :end_date, presence: true #TODO check: 1)second date > first date  2)at least one date is not blank
   end
   class ReportByCategory < Report
@@ -29,6 +29,18 @@ class ReportsController < ApplicationController
   end
 
   def report_by_dates
+    @report = ReportByDates.new(@report_params)
+    if @report.valid?
+      @report.otype_name = Otype.find(@report.otype_id).title
+      @report.category_name = Category.find(@report.category_id).name
+      selected_operations = Operation.select(:id, :amount, :odate, :description).where(category: @report.category_id, otype: @report.otype_id, odate: @report.start_date..@report.end_date)
+      operations_data = selected_operations.map { |o| [o.odate.to_s, o.amount] }
+      @dates = operations_data.map { |o| o[0] }
+      @amounts = operations_data.map { |o| o[1] }
+      render :report_by_dates
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
