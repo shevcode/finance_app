8232845e4a4a938aa4dcdec3a9036b75eb65aecd
Ivan Shevtsov <shevtsov.yan@gmail.com>
2023-01-31 13:18:41 +0200

    Complete reports internationalization

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,8 +4,8 @@ class ReportsController < ApplicationController
     include ActiveModel::Model
     attr_accessor :start_date, :end_date, :otype_name, :otype_id, :category_id, :category_name, :category_groups, :total
     validates :start_date, :end_date, presence: true 
-    validates :start_date, :end_date, format: { with: /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/, message: "should be in format YYYY-MM-DD" }
-    validates :end_date, comparison: { greater_than_or_equal_to: :start_date, message: 'must be greater than or equal to start date' }
+    validates :start_date, :end_date, format: { with: /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/ }
+    validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
   end
   class ReportByCategory < Report
     validates :otype_id, presence: true
