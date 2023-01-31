454b22abca635d101857df50734fed3e611e2891
Ivan Shevtsov <shevtsov.yan@gmail.com>
2023-01-31 14:43:11 +0200

    Add uk I18n for kaminari gem

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module FinanceApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.available_locales = [:en, :uk]
     I18n.default_locale = :uk
 
     # Configuration for the application, engines, and railties goes here.
