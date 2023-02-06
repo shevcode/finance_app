fe1f920bc86e840fc70640453e12ddacce79197d
Ivan Shevtsov <shevtsov.yan@gmail.com>
2023-02-06 16:13:12 +0200

    Correct mistake in most expensive category

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
     @top3ops = Operation.select(:description, :amount, :odate).last_month.top_x(3).type_is(2)
     @profit_last_month = Operation.last_month.type_is(1).sum(:amount)
     @expenses_last_month = Operation.last_month.type_is(2).sum(:amount)
-    @most_expensive_category = Operation.joins(:category).last_month.type_is(2).group(:name).sum(:amount).max
+    @most_expensive_category = Operation.joins(:category).last_month.type_is(2).group(:name).sum(:amount).max_by{ |k,v| v }
     
   end
 end
